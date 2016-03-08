1b07d5723ec141c7d58ee56f919412bce5d008f5
Thiago P Bueno <thiago.pbueno@gmail.com>
2016-03-08 01:31:12 -0300

    Refactor main.cpp functions and add query prompt

--- code/main.cpp
+++ code/main.cpp
@@ -3,6 +3,7 @@ using namespace bn;
 
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <unordered_map>
 #include <regex>
@@ -15,6 +16,11 @@ usage(const char *progname);
 void
 read_options(unordered_map<string,bool> &options,  int argc, char *argv[]);
 
+void
+prompt(const BN &model);
+
+void
+execute(const BN &model, string target, string evidence);
 
 int
 main(int argc, char *argv[])
@@ -36,21 +42,12 @@ main(int argc, char *argv[])
 	unsigned order;
 	BN *model;
 	read_uai_model(model_filename, order, &model);
-
 	if (options["verbose"]) {
+		cout << ">> Model:" << endl;
 		cout << *model << endl;
-		// cout << ">> Full joint distribution" << endl;
-		// cout << model->joint_distribution() << endl;
-
-		cout << ">> Markov independence" << endl;
-		for (auto pv : model->variables()) {
-			cout << *pv << endl;
-			unordered_set<const Variable*> target = model->markov_independence(pv);
-			target.insert(pv);
-			Factor f = model->query(target, model->parents(pv));
-			cout << f << endl << endl;
-		}
 	}
+
+	prompt(*model);
 
 	delete model;
 
@@ -82,4 +79,63 @@ read_options(unordered_map<string,bool> &options, int argc, char *argv[])
 			options["verbose"] = true;
 		}
 	}
+}
+
+void
+prompt(const BN &model)
+{
+	cout << ">> Query prompt:" << endl;
+	regex query_regex("query ([0-9](\\s*,\\s*[0-9])*)\\s*(\\|\\s*([0-9](\\s*,\\s*[0-9])*))?");
+	regex whitespace_regex("\\s");
+	regex quit_regex("quit");
+	while (cin) {
+		string line;
+		std::smatch str_match_result;
+		cout << "? ";
+		getline(cin, line);
+		if (regex_match(line, str_match_result, query_regex)) {
+			string target = str_match_result[1];
+			target = regex_replace(target, whitespace_regex, "");
+			string evidence = str_match_result[4];
+			evidence = regex_replace(evidence, whitespace_regex, "");
+			execute(model, target, evidence);
+		}
+		else if (regex_match(line, quit_regex)) {
+			break;
+		}
+		else {
+			cout << "Error: not a valid query." << endl;
+		}
+	}
+}
+
+void
+execute(const BN &model, string target, string evidence)
+{
+	unordered_set<const Variable*> target_vars;
+	string var = "";
+	for (char& c: target) {
+		if (c != ',') var += c;
+		else {
+			target_vars.insert(model.variables()[stoi(var)]);
+			var = "";
+		}
+	}
+	target_vars.insert(model.variables()[stoi(var)]);
+
+	unordered_set<const Variable*> evidence_vars;
+	if (evidence != "") {
+		var = "";
+		for (char& c: evidence) {
+			if (c != ',') var += c;
+			else {
+				evidence_vars.insert(model.variables()[stoi(var)]);
+				var = "";
+			}
+		}
+		evidence_vars.insert(model.variables()[stoi(var)]);
+	}
+
+	Factor q = model.query(target_vars, evidence_vars);
+	cout << q << endl;
 }
